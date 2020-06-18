7a10ac0a5d237c427e98d235c7780054c657a0c1
smadani2 <smadani2@illinois.edu>
2020-06-17 21:34:12 -0400

    updated pattern

--- utils/pattern.py
+++ utils/pattern.py
@@ -2,7 +2,7 @@ from converter import deg_to_u, range_in_deg
 from cmath import exp
 from math import radians, pi
 
-def compute_pattern(res=0.1, N=16, k=1, weights=None, single_patterns=None, calibration=None):
+def compute_pattern(res=0.1, N=16, k=1, weights=None, single_patterns=None, calibration=None, degrees=None):
     """Computes the pattern absolute value of the given parameters.
 
     res: the computation resolution. 
@@ -10,13 +10,15 @@ def compute_pattern(res=0.1, N=16, k=1, weights=None, single_patterns=None, cali
     weights: the user-selected set of weights for creating a special pattern.
     single_pattern: the pattern of each single antenna element.
     calibraiton: the calibartion values of each antenna element. 
+    degrees: the degrees for which the pattern should be computed.
 
     weights and single_patterns are assumed to be normalized. 
-    Calibration values are assumed to be in degrees. 
+    calibration values are assumed to be in degrees. 
 
     """
-    
-    degrees = range_in_deg(res)
+    if degrees is None:
+        degrees = range_in_deg(res)
+
     total_length = len(degrees)
     
     if weights is None:
@@ -31,6 +33,7 @@ def compute_pattern(res=0.1, N=16, k=1, weights=None, single_patterns=None, cali
 
     assert len(weights) == len(calibration) == len(single_patterns) == N, \
          "some vector here has the wrong length! (weights, calibration, single_patterns)"
+
 
     pattern = [abs(sum([weights[ant_i] * exp(-1j * (k * pi * u * ant_i - calibration_rad[ant_i])) \
      for ant_i in range(N)])) for u in cos_degs]
