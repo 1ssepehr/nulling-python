196f344e722c10d88fe7a50d7386580ce235b2c7
Sepehr Madani <ssepehrmadani@gmail.com>
2020-06-21 11:59:49 -0400

    Updated display_char.py dynamically

--- utils/display_chart.py
+++ utils/display_chart.py
@@ -8,6 +8,7 @@ class MainWindow(QMainWindow):
     def __init__(self, widget, parent=None):
         super(MainWindow, self).__init__()
         self.setWindowTitle("Parameter Simulation")
+        self.resize(600, 400)
         self.setCentralWidget(widget)
 
 
@@ -17,43 +18,24 @@ class VariableWidget(QWidget):
 
         # Right
         self.right = QVBoxLayout()
+        self.variableSet = ['A', 'B', 'C', 'D', 'E']
 
-        self.lbl1 = QLabel("Var A = 50")
-        self.initLabel(self.lbl1)
+        self.labels = []
+        self.sliders = []
 
-        self.lbl2 = QLabel("Var B = 50")
-        self.initLabel(self.lbl2)
+        for i in range(len(self.variableSet)):
+            self.labels.append(self.newLabel(self.variableSet[i]))
+            self.sliders.append(self.newSlider())
 
-        self.lbl3 = QLabel("Var C = 50")
-        self.initLabel(self.lbl3)
-
-        self.sl1 = QSlider(Qt.Horizontal)
-        self.initSlider(self.sl1)
-        
-        self.sl2 = QSlider(Qt.Horizontal)
-        self.initSlider(self.sl2)
-        
-        self.sl3 = QSlider(Qt.Horizontal)
-        self.initSlider(self.sl3)
-
-        self.right.addWidget(self.lbl1)
-        self.right.addWidget(self.sl1)
-        
-        self.right.addWidget(self.lbl2)
-        self.right.addWidget(self.sl2)
-        
-        self.right.addWidget(self.lbl3)
-        self.right.addWidget(self.sl3)
+        for label, slider in zip(self.labels, self.sliders):
+            self.right.addWidget(label)
+            self.right.addWidget(slider)
+            slider.valueChanged.connect(self.update_parameters)
 
         # Left
         self.left = QVBoxLayout()
         self.lbl4 = QLabel("Placeholder")
         self.left.addWidget(self.lbl4)
-
-        # Signals and Slots
-        self.sl1.valueChanged.connect(self.update_parameters)
-        self.sl2.valueChanged.connect(self.update_parameters)
-        self.sl3.valueChanged.connect(self.update_parameters)
 
         # Final Layout
         self.layout = QHBoxLayout()
@@ -61,26 +43,30 @@ class VariableWidget(QWidget):
         self.layout.addLayout(self.left)
         self.setLayout(self.layout)
 
-    def initSlider(self, slider):
-        slider.setMinimum(0)
-        slider.setMaximum(100)
-        slider.setValue(50)
-        slider.setTickPosition(QSlider.TicksBelow)
-        slider.setTickInterval(10)
-
-    def initLabel(self, label):
-        label.setAlignment(Qt.AlignCenter)
-
+    
     @Slot()
     def update_parameters(self):
-        SliderLabelPairs = [(self.sl1, self.lbl1), (self.sl2, self.lbl2), (self.sl3, self.lbl3)]
-        for slider, label in SliderLabelPairs:
+        for label, slider in zip(self.labels, self.sliders):
             newValue = slider.value()
             currentText = str(label.text())
             valueIndex = currentText.find('=') + 2 # the index of the number in the label
             newText = currentText[:valueIndex] + str(newValue)
             label.setText(newText)
 
+    def newLabel(self, name):
+        labelText = 'Var {} = {}'.format(name, 50)
+        label = QLabel(labelText)
+        label.setAlignment(Qt.AlignCenter)
+        return label
+
+    def newSlider(self):
+        slider = QSlider(Qt.Horizontal)
+        slider.setMinimum(0)
+        slider.setMaximum(100)
+        slider.setValue(50)
+        slider.setTickPosition(QSlider.TicksBelow)
+        slider.setTickInterval(10)
+        return slider
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
