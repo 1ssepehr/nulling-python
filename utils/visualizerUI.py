9405106b00cf778f85577027aa448b991345851b
Sepehr Madani <ssepehrmadani@gmail.com>
2020-07-02 10:39:08 -0400

    Added parameters and ploting

--- utils/visualizerUI.py
+++ utils/visualizerUI.py
@@ -1,26 +1,34 @@
+from cmath import exp
+from math import log10, pi
+
 from PySide2.QtCore import QCoreApplication, QMetaObject, QRect, QSize, Qt
 from PySide2.QtWidgets import *
-
 from matplotlib.backends.backend_qt5agg import FigureCanvas, NavigationToolbar2QT
 from matplotlib.figure import Figure
 
-import numpy as np
+from pattern import compute_pattern, range_in_deg
+
+
+class Parameter:
+    def __init__(self, name, value, min=0.0, max=1.0):
+        self.name = name
+        self.value = value
+        self.min = min
+        self.max = max
+        
+    def scaleTo100Ratio(self, value) -> int:
+        return int(100 * ((value - self.min) / (self.max - self.min)))
+
+    def scaleFrom100Ratio(self, ratio_value) -> float:
+        return self.min + (ratio_value / 100) * (self.max - self.min)
 
 class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
+    def setupUi(self, MainWindow, parameterSet):
 
         # Define Size Policies
         prefSizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        prefSizePolicy.setHorizontalStretch(0)
-        prefSizePolicy.setVerticalStretch(0)
-
         fixedSizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Preferred)
-        fixedSizePolicy.setHorizontalStretch(0)
-        fixedSizePolicy.setVerticalStretch(0)
-        
         expandingSizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-        expandingSizePolicy.setHorizontalStretch(0)
-        expandingSizePolicy.setVerticalStretch(0)
 
         # Set up Main Window
         if not MainWindow.objectName():
@@ -31,7 +39,7 @@ class Ui_MainWindow(object):
         MainWindow.resize(1280, 720)
 
         # Parameters
-        self.parameterSet = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+        self.parameterSet = parameterSet
         
         # Menu Bar actions (Top)
         self.menubar = QMenuBar(MainWindow)
@@ -56,7 +64,7 @@ class Ui_MainWindow(object):
         self.menuHelp.addAction(self.actionAbout)
         MainWindow.setMenuBar(self.menubar)
 
-        # Status Bar
+        # Status Bar (Bottom)
         self.statusbar = QStatusBar(MainWindow)
         MainWindow.setStatusBar(self.statusbar)
         
@@ -65,20 +73,19 @@ class Ui_MainWindow(object):
         self.labelList = []
         self.sliderList = []
         self.lineEditList = []
-
         self.toolboxLayout = QVBoxLayout()
 
         for parameter in self.parameterSet:
             # Label
             newLabel = QLabel(self.centralwidget)
             newLabel.setSizePolicy(prefSizePolicy)
-            newLabel.setText("Var {}".format(parameter))
+            newLabel.setText("Var {}".format(parameter.name))
 
             # Line Edit
             newLineEdit = QLineEdit(self.centralwidget)
             newLineEdit.setSizePolicy(fixedSizePolicy)
-            newLineEdit.setMaximumSize(QSize(32, 16777215))
-            newLineEdit.setText("50")
+            newLineEdit.setMaximumSize(QSize(48, 16777215))
+            newLineEdit.setText(str(parameter.value))
             newLineEdit.textChanged.connect(lambda: self.update_parameters(source=QLineEdit))
             newLabel.setBuddy(newLineEdit)
 
@@ -86,11 +93,13 @@ class Ui_MainWindow(object):
             newSlider = QSlider(Qt.Horizontal, self.centralwidget)
             newSlider.setSizePolicy(expandingSizePolicy)
             newSlider.setMinimumSize(QSize(100, 0))
-            newSlider.setValue(50)
+            newSlider.setRange(0, 100)
+            newSlider.setValue(parameter.scaleTo100Ratio(parameter.value))
             newSlider.setTickPosition(QSlider.TicksBelow)
             newSlider.setTickInterval(5)
             newSlider.valueChanged.connect(lambda: self.update_parameters(source=QSlider))
 
+            # Bundle layout for label, line edit, and slider
             newControlLayout = QHBoxLayout()
             newControlLayout.addWidget(newLabel)
             newControlLayout.addWidget(newLineEdit)
@@ -103,6 +112,11 @@ class Ui_MainWindow(object):
             
             self.toolboxLayout.addLayout(newControlLayout)
 
+        # Checkbox for using log10
+        self.useLog10Checkbox = QCheckBox("Use log10 for graph", self.centralwidget)
+        self.useLog10Checkbox.setChecked(True)
+        self.toolboxLayout.addWidget(self.useLog10Checkbox)
+
         self.toolboxSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
         self.toolboxLayout.addItem(self.toolboxSpacer)
 
@@ -112,13 +126,12 @@ class Ui_MainWindow(object):
         self.plotWidget.setMinimumSize(QSize(720, 480))
         self.plotLayout = QVBoxLayout(self.plotWidget)
 
-        self.staticCanvas = FigureCanvas(Figure(figsize=(5, 3)))
-        MainWindow.addToolBar(NavigationToolbar2QT(self.staticCanvas, MainWindow))
-        self.staticAxes = self.staticCanvas.figure.subplots()
-        t = np.linspace(0, 10, 501)
-        self.staticAxes.plot(t, np.tan(t))
+        self.canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        MainWindow.addToolBar(NavigationToolbar2QT(self.canvas, MainWindow))
+        self.chart = self.canvas.figure.subplots()
+        self.plotLayout.addWidget(self.canvas)
 
-        self.plotLayout.addWidget(self.staticCanvas)
+        self.useLog10Checkbox.stateChanged.connect(self.update_chart)
 
         # Window layout (MainWindow)
         self.centralLayout = QHBoxLayout(self.centralwidget)
@@ -131,16 +144,46 @@ class Ui_MainWindow(object):
         QMetaObject.connectSlotsByName(MainWindow)
 
     def update_parameters(self, source):
-        for slider, lineEdit in zip(self.sliderList, self.lineEditList):
+        for slider, lineEdit, parameter in zip(self.sliderList, self.lineEditList, self.parameterSet):
             if source == QSlider:
-                lineEdit.setText(str(slider.value()))
+                newValue = parameter.scaleFrom100Ratio(slider.value())
+                parameter.value = newValue
+                lineEdit.setText(str(newValue))
             if source == QLineEdit:
                 try:
-                    newValue = int(lineEdit.text())
-                    slider.setValue(newValue)
+                    newValue = float(lineEdit.text())
+                    parameter.value = newValue
+                    slider.setValue(parameter.scaleTo100Ratio(newValue))
                 except ValueError:
                     pass
+        self.update_chart()
+
+    def update_chart(self):
+        self.chart.clear()
+        self.chart.set_xticks([15 * x for x in range(13)])
+        self.chart.set_xlabel("Degrees (°)")
+        self.chart.set_ylabel("dB")
+
+        customWeights = [exp(2 * pi * 1j * x.value) for x in self.parameterSet] + ([1] * (16 - len(self.parameterSet)))
+        data_x = compute_pattern(weights=customWeights)
+        
+        for elem in data_x:
+            if elem < 0:
+                raise ValueError("negative values in list 'data_x'")
     
+        if(self.useLog10Checkbox.isChecked()):
+            data_x = list(map(lambda x: 10*log10(x), data_x))
+            self.chart.set_ylim(-30, 10 * log10(16) + 1)
+        else:
+            self.chart.set_ylim(0, 18)
+
+        data_y = range_in_deg(0.1)
+
+        if len(data_x) != len(data_y): # size of each dataset must be N = 1 + (180/res)
+            raise ValueError("resolution doesn't match with data_x's length")
+
+        self.chart.plot(data_y, data_x)
+        self.chart.figure.canvas.draw()
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Chart Viewer", None))
